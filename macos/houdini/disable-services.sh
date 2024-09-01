1153b3d7119e471afff485b2b333c720150741a7
Fernando Moreira <fernando@parteincerta.com>
2024-09-01 23:48:31 +0100

    Disable another set of macOS services in `houdini`

--- macos/houdini/disable-services.sh
+++ macos/houdini/disable-services.sh
@@ -9,7 +9,9 @@ system_services=(
 	com.apple.biomed
 	com.apple.mobile.obliteration # Remote device wipe functionality
 	com.apple.ospredictiond       # Predicts OS conditions
+	com.apple.siriinferenced
 	com.apple.touchbarserver
+	com.apple.triald.system
 )
 for item in "${system_services[@]}"; do
 	echo "Disabling system/$item ..."
@@ -29,10 +31,25 @@ user_services=(
   com.apple.appstoreagent
   com.apple.appstorecomponentsd
   com.apple.assistantd
+  com.apple.bird
+  com.apple.cloudd
+  com.apple.cmio.ContinuityCaptureAgent
+  com.apple.commerce
   com.apple.gamed
   com.apple.homed
   com.apple.imagent
+  com.apple.email.maild
+  com.apple.parsec-fbf
+  com.apple.parsecd
+  com.apple.photoanalysisd
+  com.apple.photolibraryd
+  com.apple.routined
+  com.apple.security.keychain-circle-notification
+  com.apple.siriactionsd
+  com.apple.storedownloadd
+  com.apple.studentd
   com.apple.suggestd
+  com.apple.triald
 )
 for item in "${user_services[@]}"; do
 	echo "Disabling user/501/$item ..."
