415824cae66c970feffc6215dbf15529d815c943
Fernando Moreira <fernando@parteincerta.com>
2024-10-05 02:36:45 +0100

    Improve install-hosts.sh and update exclusion list

--- shared/scripts/install-hosts.sh
+++ shared/scripts/install-hosts.sh
@@ -21,51 +21,61 @@ if [ "$system" = "Darwin" ]; then
 		-o "$TMPDIR/hosts"
 
 	echo "-> Applying additions ..."
-	# ------------------------------
 
 	hosts_additions_orig="$rootdir/shared/scripts/install-hosts-additions.json"
 	hosts_additions_subst="${TMPDIR}hosts-additions.json"
 	export hostname="${1:-${HOSTNAME%.*}}"
 	envsubst <"$hosts_additions_orig" >"$hosts_additions_subst"
 
-	echo -e "\n# START --- General additions" >>"$TMPDIR/hosts"
-	shared_address_list=$(jq -r ".shared | keys[]" "$hosts_additions_subst")
+	echo "" >>"$TMPDIR/hosts"
+	echo "# START --- General additions" >>"$TMPDIR/hosts"
+	mapfile -d '' shared_address_list < \
+		<(jq --raw-output0 '.shared | keys[]' "$hosts_additions_subst")
 	for addr in "${shared_address_list[@]}"; do
-		shared_names_list=$(jq -r ".shared[\"$addr\"][]" "$hosts_additions_subst")
+		mapfile -d '' shared_names_list < \
+			<(jq --raw-output0 ".shared[\"$addr\"][]" "$hosts_additions_subst")
 		for name in "${shared_names_list[@]}"; do
-			echo -e "$addr $name" >>"$TMPDIR/hosts"
+			echo "$addr $name" >>"$TMPDIR/hosts"
 		done
 	done
-	echo -e "# END --- General additions" >>"$TMPDIR/hosts"
+	echo "# END --- General additions" >>"$TMPDIR/hosts"
 
 	if [ -n "$1" ]; then
-		echo -e "\n# START --- Specific additions for $1" >>"$TMPDIR/hosts"
-		specific_address_list=$(jq -r ".specific.$1 // {} | keys[]" "$hosts_additions_subst")
+		echo "" >>"$TMPDIR/hosts"
+		echo "# START --- Specific additions for $1" >>"$TMPDIR/hosts"
+		mapfile -d '' specific_address_list < \
+			<(jq --raw-output0 ".specific.$1 | keys[]" "$hosts_additions_subst")
 		for addr in "${specific_address_list[@]}"; do
 			if [ -z "$addr" ]; then continue; fi
-			specific_names_list=$(jq -r ".specific.$1[\"$addr\"][]" "$hosts_additions_subst")
+			mapfile -d '' specific_names_list < \
+				<(jq --raw-output0 ".specific.$1[\"$addr\"][]" \
+					"$hosts_additions_subst")
 			for name in "${specific_names_list[@]}"; do
-				echo -e "$addr $name" >>"$TMPDIR/hosts"
+				echo "$addr $name" >>"$TMPDIR/hosts"
 			done
 		done
-		echo -e "# END --- Specific additions for $1" >>"$TMPDIR/hosts"
+		echo "# END --- Specific additions for $1" >>"$TMPDIR/hosts"
 	fi
 
 	echo "-> Applying exclusions ..."
-	# -------------------------------
 
-	shared_address_list=$(jq -r '.shared | join(" ")' "$rootdir/shared/scripts/install-hosts-exclusions.json")
-	for name in $shared_address_list; do
+	mapfile -d '' shared_address_list < \
+		<(jq --raw-output0 '.shared[]' \
+			"$rootdir/shared/scripts/install-hosts-exclusions.json")
+	for name in "${shared_address_list[@]}"; do
 		if [ -z "$name" ]; then continue; fi
 		echo -e "\t-> Excluding $name ..."
 		sed -i '' -r "/$name/s//# &/" "$TMPDIR/hosts"
 	done
 
 	if [ -n "$1" ]; then
-		specific_address_list=$(jq -r ".specific.$1 // [] | join(\" \")" "$rootdir/shared/scripts/install-hosts-exclusions.json")
-		for name in $specific_address_list; do
+		mapfile -d '' specific_address_list < \
+			<(jq --raw-output0 ".specific.$1[]" \
+				"$rootdir/shared/scripts/install-hosts-exclusions.json")
+		for name in "${specific_address_list[@]}"; do
 			if [ -z "$name" ]; then continue; fi
-			sed -i '' -r "/^$name/s//#&/" "$TMPDIR/hosts"
+			echo -e "\t-> Excluding $name ..."
+			sed -i '' -r "/$name/s//# &/" "$TMPDIR/hosts"
 		done
 	fi
 
