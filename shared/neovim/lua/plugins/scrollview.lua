61539023cbd019091244fd6ade625046d1d395c8
Fernando Moreira <fernando@parteincerta.com>
2024-10-24 20:00:58 +0100

    Update nvim's scrollview plugin to v6.*

--- shared/neovim/lua/plugins/scrollview.lua
+++ shared/neovim/lua/plugins/scrollview.lua
@@ -4,16 +4,16 @@
 return {
 	'dstein64/nvim-scrollview',
 	enabled = true,
-	version = "v5.*",
-	config = function ()
+	version = "v6.*",
+	config = function()
 		local scrollview = require('scrollview')
 		scrollview.setup({
 			base = 'right',
 			current_only = true,
-			diagnostics_severities = {vim.diagnostic.severity.ERROR},
-			excluded_filetypes = {'neo-tree', 'fzf-lua'},
+			diagnostics_severities = { vim.diagnostic.severity.ERROR },
+			excluded_filetypes = { 'neo-tree', 'fzf-lua' },
 			floating_windows = false,
-			signs_on_startup = {'cursor', 'diagnostics', 'search'},
+			signs_on_startup = { 'cursor', 'diagnostics', 'search' },
 			winblend = 20,
 		})
 	end
