d1cd0911bab65c60aa23fef96491d173c7d02226
Fernando Moreira <fernando@parteincerta.com>
2025-03-31 02:21:09 +0100

    Revise `disable-services.sh` for a few other host
    `mbp16-m2m`, `mm-m2p` and `ms-m1u`.

--- macos/mbp16-m2m/other/disable-services.sh
+++ macos/mbp16-m2m/other/disable-services.sh
@@ -1,63 +1,65 @@
 #!/usr/bin/env bash
 
-# System Services
 system_services=(
-	# Diagnostics and usage data collection
+	com.apple.GameController.gamecontrollerd
 	com.apple.analyticsd
-
-	# Predicts OS conditions
+	com.apple.backupd
+	com.apple.backupd-helper
+	com.apple.familycontrols
 	com.apple.ospredictiond
-
-	# Diagnostics and usage reporting
 	com.apple.rtcreportingd
-
-	# Siri-related daemons
+	com.apple.security.syspolicy
 	com.apple.siriinferenced
-
-	# Primarily involved in gathering data from experiments
-	# assigned through CloudKit.
 	com.apple.triald.system
+	com.apple.wifianalyticsd
 )
 for item in "${system_services[@]}"; do
 	echo "Disabling system/$item ..."
+	sudo launchctl bootout "system/$item"
 	sudo launchctl disable "system/$item"
 done
 
-# User Services
-
-# NOTE: Don't disable the following ones:
-# com.apple.bird      -> Annoying message about iCloud pops up randomly.
-# com.apple.contactsd -> Spotlight search and share stop working.
-
 user_services=(
-	# Diagnostics and usage reporting
 	com.apple.DiagnosticsReporter
+	com.apple.GameController.gamecontrolleragentd
 	com.apple.ReportCrash
-
-	# Apple Pay and Wallet daemon
+	com.apple.SiriTTSTrainingAgent
+	com.apple.Siri.agent
+	com.apple.TMHelperAgent
+	com.apple.TMHelperAgent.SetupOffer
+	com.apple.UsageTrackingAgent
+	com.apple.WiFiVelocityAgent
+	com.apple.ap.adprivacyd
+	com.apple.ap.adservicesd
+	com.apple.ap.promotedcontentd
+	com.apple.assistant_service
+	com.apple.assistantd
+	com.apple.betaenrollmentd
+	com.apple.corespeechd
+	com.apple.familycircled
+	com.apple.familycontrols.useragent
+	com.apple.familynotificationd
 	com.apple.financed
-
-	# GameKit services
 	com.apple.gamed
-
-	# Classroom's student control agent
-	com.apple.studentd
-
-	# Primarily involved in gathering data from experiments
-	# assigned through CloudKit.
-	com.apple.triald
-
-	# Siri-related daemons
+	com.apple.intelligenceplatformd
+	com.apple.knowledge-agent
+	com.apple.macos.studentd
+	com.apple.newsd
+	com.apple.parsec-fbf
+	com.apple.parsecd
+	com.apple.routined
+	com.apple.siri.context.service
 	com.apple.siriactionsd
 	com.apple.siriknowledged
 	com.apple.sirittsd
-	com.apple.SiriTTSTrainingAgent
-	com.apple.triald
-
+	com.apple.suggestd
 	com.apple.tipsd
+	com.apple.triald
 )
+
 uid=$(id -u)
 for item in "${user_services[@]}"; do
 	echo "Disabling user/$uid/$item ..."
+	launchctl bootout "user/$uid/$item"
 	launchctl disable "user/$uid/$item"
 done
