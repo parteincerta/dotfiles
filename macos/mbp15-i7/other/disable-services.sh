67c31a24fb5bcf410045093d0df42c010ffd7fb5
Fernando Moreira <fernando@parteincerta.com>
2025-05-17 17:20:08 +0100

    Stop disabling `syspolicyd` for some hosts
    Disabling `syspolicyd` for `mbp13-i7` and `mbp15-i7` causes a problem
    with OpenCore Legacy Patcher where the patched drivers are not properly
    loaded.

--- macos/mbp15-i7/other/disable-services.sh
+++ macos/mbp15-i7/other/disable-services.sh
@@ -30,7 +30,6 @@ system_services=(
 	com.apple.protectedcloudstorage.protectedcloudkeysyncing
 	com.apple.rapportd
 	com.apple.security.cloudkeychainproxy3
-	com.apple.security.syspolicy
 	com.apple.siri.morphunassetsupdaterd
 	com.apple.siriinferenced
 	com.apple.softwareupdated
