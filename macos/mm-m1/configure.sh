197a7ffcbf649f9a07667a325af45e18f4b55370
Fernando Moreira <fernando@parteincerta.com>
2025-07-12 23:24:29 +0100

    Add applications settings for `mm-m1`
    Activity Monitor, BetterDisplay and OBS Studio.

--- macos/mm-m1/configure.sh
+++ macos/mm-m1/configure.sh
@@ -78,7 +78,7 @@ cp "$rootdir/shared/tokyonight-moon.tmTheme" "$XDG_CONFIG_HOME/bat/themes"
 cp "$rootdir/shared/zed.keymap.json" "$XDG_CONFIG_HOME/zed/keymap.json"
 cp "$rootdir/shared_macos/config.fish" "$XDG_CONFIG_HOME/fish/"
 cp "$rootdir/shared_macos/lfrc" "$XDG_CONFIG_HOME/lf/"
-# cp -R obs/* "$app_support_folder/obs-studio/basic"
+cp -R obs/* "$app_support_folder/obs-studio/basic"
 cp other/mise.toml "$XDG_CONFIG_HOME/mise/config.toml"
 
 chmod u=rwx,g=,o= "$HOME/.gnupg"
@@ -93,9 +93,9 @@ touch "$HOME/.hushlogin"
 touch "$XDG_CONFIG_HOME/lf/bookmarks"
 
 source "$rootdir/shared_macos/scripts/export-defaults.sh" --source-keys-only
-# defaults import "$actmon_key" "$actmon_file"
+defaults import "$actmon_key" "$actmon_file"
 defaults import "$alttab_key" "$alttab_file"
-# defaults import "$betterdisplay_key" "$betterdisplay_file"
+defaults import "$betterdisplay_key" "$betterdisplay_file"
 cp "$macmousefix_file" "$app_support_folder/com.nuebling.mac-mouse-fix/config.plist"
 
 # This section is reserved for files that must be patched upfront.
