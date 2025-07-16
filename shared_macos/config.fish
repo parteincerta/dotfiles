6619b7df2e6892ba2d20349b922f57eb1f6e2cc0
Fernando Moreira <fernando@parteincerta.com>
2025-07-16 14:46:43 +0100

    Revert 22bae89.
    This reverts commit 22bae8951227972e239c48d2cfde6453b1e646da.
    Turns out that this issue was related with Homebrew (on Apple Silicon)
    adding itself to the `$PATH` by adding an entry in `/etc/paths.d/`.
    We've explicitly removed this entry in 4144568.

--- shared_macos/config.fish
+++ shared_macos/config.fish
@@ -207,25 +207,10 @@ function prompt_cwd
 	set_color $fish_color_normal
 end
 
-function prompt_lf_level
-	if [ -n "$LF_LEVEL" ]
-		set_color black --background white
-		echo -n " $LF_LEVEL "
-		set_color $fish_color_normal
-		echo -n " "
-	end
-end
-
 function prompt_sh_level
 	if [ "$SHLVL" -gt "1" ]
-		if [ -n "$LF_LEVEL" ]
-			set --function lvl (math $SHLVL - $LF_LEVEL - 1)
-		else
-			set --function lvl (math $SHLVL - 1)
-		end
-
 		set_color black --background white
-		echo -n " $lvl "
+		echo -n " $(math $SHLVL - 1) "
 		set_color $fish_color_normal
 		echo -n " "
 	end
