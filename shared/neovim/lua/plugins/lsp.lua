d668507bbe8931240d4c1fbc2b484894fd61ad74
Fernando Moreira <fernando@parteincerta.com>
2025-08-06 11:10:34 +0100

    Change `nvim-lspconfig` version tracking to `v2.*`

--- shared/neovim/lua/plugins/lsp.lua
+++ shared/neovim/lua/plugins/lsp.lua
@@ -139,7 +139,7 @@ return {
 		{ "hrsh7th/cmp-nvim-lsp" },
 		{
 			"neovim/nvim-lspconfig",
-			tag = "v2.3.0",
+			version = "v2.*",
 			config = function()
 				vim.diagnostic.config({
 					virtual_text = false,
